8051367283011a87b1d15ed7a5c4524a7dce5312
Mayank Tiwari <mayank_tiwari@epam.com>
2021-12-14 16:50:30 +0530

    Added comment to start code

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -8,24 +8,7 @@ function App(props) {
     
 return(
   <div className="container">
-     <div className="content-block"> 
-   <img className="avatar" src={employee.profileImg} alt={employee.name} />
- </div>
- <div className="content-block"> 
-   <label className="jumbotron">{employee.name}</label>
- </div>
- <div className="content-block"> 
- <p className="labels">Location: </p>
- <label>{employee.location}</label>
-</div>
-<div className="content-block"> 
-   <p className="labels">Blood Group: </p>
-   <label>{employee.bloodGroup}</label>
- </div>
- <div className="content-block"> 
- <p className="labels">Age: </p>
- <label>{employee.age}</label>
-</div>
+  {/* Start your React code here */}
 </div>
 )
 }
