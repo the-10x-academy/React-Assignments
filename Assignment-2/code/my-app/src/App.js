0ee5a4f8f60a87d17ba33b534a81b3e86237a405
lalatendupany <lalatendupany.2000@gmail.com>
2022-04-04 20:26:19 +0530

    completed

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -1,15 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
+import './index.css';
 
 
 
 function App(props) {
  const {employee}=props;
+ 
+
     
 return(
+
   <div className="container">
+    <img className='img1' src={employee.profileImg} alt='profileimg' />
+    <div className='name'>{employee.name}</div>
+    <p className='static'>Location</p>
+    <div className='dynamic'>{employee.location}</div>
+    <p className='static'>BloodGroup</p>
+    <div className='dynamic'>{employee.bloodGroup}</div>
+    <p className='static'>Age</p>
+    <div className='dynamic'>{employee.age}</div>
   {/* Start your React code here */}
 </div>
+
+  
+  
 )
 }
 
