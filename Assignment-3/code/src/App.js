ae4292d6e960cf64789b68c11c3c8045c070764c
user name <user email>
2022-05-17 12:16:58 +0530

    Assignment-3

--- Assignment-3/code/src/App.js
+++ Assignment-3/code/src/App.js
@@ -1,15 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./App.css";
 
+const arrow="<"
 
-var arrow="<"
-function App() {
+const App = (props)=>{
+  const {information} = props
+  const {Name,EmployeeID,Time,date,Email,Phone,Status,Door,time2} = information;
   return (
     <div className="site-container">
     {/* Start Your code here */}
+      <nav className='child' id='customer-row'>
+        <span className='arrow'>{arrow}</span>
+        <span>
+          <p><strong>{Name}</strong></p>
+          <p style={{color:'gray'}}>{EmployeeID}</p>
+        </span>
+        <button>Print</button>
+      </nav>
+
+      <div className='child' id='customer-info'>
+        <p><strong>Appointment{' '}</strong>{Time} ({date}) </p>
+        <p><strong>Email {' '}</strong>{Email}</p>
+        <p><strong>Phone {' '}</strong>{Phone}</p>
+      </div>
+
+      <div className='child' id='order-info'>
+        <table>
+          <thead>
+            <tr>
+              <th>Status</th>
+              <th>Door</th>
+              <th>Time</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+              <td><span id='bullet'>bull</span> {Status}</td>
+              <td>{Door}</td>
+              <td>{time2} <span style={{color:'gray'}}>({date})</span> </td>
+            </tr>
+            </tbody>
+        </table>
+      </div>
+
+      <div className='child' id='product-list'>
+        <button></button>
+        <img src='https://www.w3schools.com/howto/img_avatar.png' alt=''/>
+        <span>
+        <h3>Commodi provident</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis vitae inventore illo laudantium aliquid culpa totam fuga esse ab itaque minus, nemo, non quidem officia. Quisquam placeat officiis, natus exercitationem perspiciatis excepturi ipsa vitae explicabo nulla voluptates. Perferendis, cupiditate fugit.</p>
+        </span>
+        <p className='arr'>{'>'}</p>
+      </div>
     </div>
   );
+
 }
+
 
 export default App;
--- Assignment-3/code/src/index.js
+++ Assignment-3/code/src/index.js
@@ -4,13 +4,26 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+const information = {
+  Name:"Alan Ford",
+  EmployeeID:"00005152",
+  Time:"9:00",
+  date:"24-05-2016",
+  Email:"alan.ford@email.nl",
+  Phone:"+31123456789",
+  Status:"In Progress",
+  Door:"mark",
+  time2:"10:30",
+  Img:"https://www.w3schoola.com/howto/ing_avatar.png",
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App information={information} />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
