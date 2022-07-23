f448fb85de63216034b602e440c74b2b46c867b1
user name <user email>
2022-07-23 19:36:32 +0530

    Assignment-3

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -1,16 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
 
 
 
 function App(props) {
- const {employee}=props;
+  const {employee}=props;
     
 return(
   <div className="container">
-  {/* Start your React code here */}
+  {
+  
+  /* Start your React code here */ 
+  <center>
+        <div className="box">
+
+          <div className="image">
+            <img src={employee.profileImg} alt="" className="profile-image"></img>
+          </div>
+
+          <div className="main_name">
+            <p className="name">{employee.name}</p>
+          </div>
+
+          <div className="Address">
+            <p>location</p>
+            <h3>{employee.location}</h3>
+          </div>
+
+          <div className="BloodGroup">
+            <p>Blood Group</p>
+            <h3>{employee.bloodGroup}</h3>
+          </div>
+
+          <div className="age">
+            <p>age</p>
+            <h3>{employee.age}</h3>
+          </div>
+
+        </div>
+
+  </center>
+
+
+  }
 </div>
+
 )
+
 }
 
 
